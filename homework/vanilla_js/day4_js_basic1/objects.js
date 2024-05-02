b22d3716b67ca9150d76ba5efcbc7bf7a2f1f34a
Felix Chen <felixchen3674@gmail.com>
2024-05-02 01:05:15 -0400

    update

--- homework/vanilla_js/day4_js_basic1/objects.js
+++ homework/vanilla_js/day4_js_basic1/objects.js
@@ -10,23 +10,26 @@ export function combineObjects(obj1, obj2) {
 }
 
 export function changeValueOf(obj, key, value) {
+  // update the obj in place, do not return a new obj
   // Change the value of the key in the object
   // Example 1: const obj = {name: 'Alice', age: 25};
   // changeValueOf(obj, 'age', 21);
-  // Expected output: {name: 'Alice', age: 21}
+  // Expected obj: {name: 'Alice', age: 21}
   // Example 2: const obj = {name: 'Alice', age: 25};
   // changeValueOf(obj, 'job', 'teacher');
-  // Expected output: {name: 'Alice', age: 25, job: 'teacher'}
+  // Expected obj: {name: 'Alice', age: 25, job: 'teacher'}
 }
 
 export function cancelExpiredEvents(events) {
+  // update the events in place, do not return a new obj
+
   // Cancel the expired events
   // Example: const events = [
   //   event1: {name: 'Birthday Party', date: '2020-01-01', isCanceled: false},
   //   event2: {name: 'New Year Party', date: '2021-01-01', isCanceled: false},
   //   event3: {name: 'Christmas Party', date: '2024-12-25', isCanceled: false}
   // ];
-  // Expected output: // an array of events, but event1 and event2 are canceled, event3 is not canceled
+  // Expected events: // an array of events, but event1 and event2 are canceled, event3 is not canceled
 }
 
 export function findEventByType(events, type) {
