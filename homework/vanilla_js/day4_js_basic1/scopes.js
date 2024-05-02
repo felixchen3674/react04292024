c52289e2812b9ebff18c1d49d18df6fce00ed126
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-05-02 13:04:13 -0700

    add scopes.js

--- homework/vanilla_js/day4_js_basic1/scopes.js
+++ homework/vanilla_js/day4_js_basic1/scopes.js
@@ -3,40 +3,40 @@
 var globalVar = "I am global";
 
 function accessGlobal() {
-  return globalVar;
+    return globalVar;
 }
 
-// console.log(accessGlobal()); // Expected output?
-// console.log(globalVar);      // Expected output?
+// console.log(accessGlobal()); // I am global
+// console.log(globalVar);      // I am global
 
 // Local scope
 function localScope() {
-  var localVar = "I am local to this function";
+    var localVar = "I am local to this function";
 }
 
 // localScope();
-// console.log(localVar); // What happens here and why?
-    
+// console.log(localVar); // Error: localVar is not defined, since var is function scoped
+
 // Block scope
 if (true) {
-  let blockVar = "I exist inside this block only";
-  const blockConst = "So do I!";
+    let blockVar = "I exist inside this block only";
+    const blockConst = "So do I!";
 }
 
-// console.log(blockVar); // What is the result and why?
-// console.log(blockConst); // What is the result and why?
+// console.log(blockVar); // not defined error, since let only active in if block.
+// console.log(blockConst); // not defined error, since const only active in if block.
 
 // Function scope vs block scope
 function loopScope() {
-  for (var i = 0; i < 3; i++) {
-    // console.log(i); // Outputs during loop?
-  }
-  //   console.log("After loop with var: " + i); // What happens here?
+    for (var i = 0; i < 3; i++) {
+        console.log(i); // can output i inside the loop
+    }
+    console.log("After loop with var: " + i); // can output i = 3 since var is function scoped
 
-  for (let j = 0; j < 3; j++) {
-    console.log(j); // Outputs during loop?
-  }
-  //   console.log("After loop with let: " + j); // What happens here?
+    for (let j = 0; j < 3; j++) {
+        console.log(j); // can output j inside the loop
+    }
+    console.log("After loop with let: " + j); // error: j is not defined, since let is block scoped.
 }
 
 loopScope();
