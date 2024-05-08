6fa5e0d75f7f222d481927e7b342a19e4a008b6a
LiZhe <1756368629@qq.com>
2024-05-07 20:12:05 -0400

    day7_functions homework submitted

--- homework/vanilla_js/jest.config.js
+++ homework/vanilla_js/jest.config.js
@@ -5,8 +5,8 @@ module.exports = {
   testMatch: [
     // "**/vanilla_js/day4_js_basic1/**/*.test.js",
     // "**/vanilla_js/day5_js_basic2/**/*.test.js",
-    '**/vanilla_js/day6_oop/**/*.test.js',
-    // "**/vanilla_js/day7_functions/**/*.test.js",
+    // '**/vanilla_js/day6_oop/**/*.test.js',
+    '**/vanilla_js/day7_functions/**/*.test.js',
     // "**/vanilla_js/day8_promise/**/*.test.js",
   ],
 };
