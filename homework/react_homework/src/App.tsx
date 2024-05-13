3b1cc4e9991cc7f9433a58f791f4d0a28b49daa1
Felix Chen <felixchen3674@gmail.com>
2024-05-13 01:21:43 -0400

    added react homework day 11

---
+++ homework/react_homework/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import { BirthdayRecord, SelectAllForm, UseRequestApp } from "./many_practices";
+import Day11Notes from "./day11_describing_the_ui/notes";
+
+function App() {
+  return (
+    <>
+      <Day11Notes />
+    </>
+  );
+}
+
+export default App;
