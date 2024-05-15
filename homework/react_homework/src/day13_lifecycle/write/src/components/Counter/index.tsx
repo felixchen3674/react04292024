387d4a3e46175de39d9636685e645111a54aa714
ziyang-666 <song.ziya@northeastern.edu>
2024-05-15 16:29:10 -0700

    update 13

---
+++ homework/react_homework/src/day13_lifecycle/write/src/components/Counter/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useRef } from 'react'
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const [startDisabled, setStartDisabled] = useState(false);
+  const [pauseDisabled, setPauseDisabled] = useState(true);
+  const intervalRef = useRef(null);
+
+  const handleStart = () => {
+    intervalRef.current = setInterval(() => {
+        setCount(prev => prev + 1);
+      }, 1000);
+    setStartDisabled(true);
+    setPauseDisabled(false);
+  }
+
+  const handlePause = () => {
+    clearInterval(intervalRef.current);
+    setStartDisabled(false);
+    setPauseDisabled(true);
+  }
+
+  const handleReset = () => {
+    clearInterval(intervalRef.current);
+    setCount(0);
+    setStartDisabled(false);
+    setPauseDisabled(true);
+  }
+
+  return (
+    <div>
+        <p>{count}</p>
+        <button disabled={startDisabled} onClick={handleStart}>Start</button>
+        <button disabled={pauseDisabled} onClick={handlePause}>Pause</button>
+        <button onClick={handleReset}>Reset</button>
+    </div>
+  )
+}
