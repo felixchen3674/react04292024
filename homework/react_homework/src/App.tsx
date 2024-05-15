34ef456e9fc3c1b3e8783cc707c8c8220151ab91
Lin He <xlhe@cs.washington.edu>
2024-05-14 17:58:53 -0700

    todolist

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -3,18 +3,14 @@ import Day11Play from "./day11_describing_the_ui/notes";
 import Checkbox from "./day12_interactivity/ControlledComponents/Checkbox";
 import RadioGroup from "./day12_interactivity/ControlledComponents/RadioGroup";
 import Day12Play from "./day12_interactivity/notes";
-import Day13Play from "./day13_lifecycles/notes";
-import Day14Play from "./day14_practice";
-import Day16Play from "./day16_pattern/notes";
+import TodoListApp from "./day12_interactivity/TodoListApp";
 
 function App() {
   return (
     <>
       {/* <Day11Play /> */}
       {/* <Day12Play /> */}
-      {/* <Day13Play /> */}
-      {/* <Day14Play /> */}
-      <Day16Play />
+      <TodoListApp />
     </>
   );
 }
