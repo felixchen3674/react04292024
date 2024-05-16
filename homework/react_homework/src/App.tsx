f2d8046504981410c0a1efb57abad72bf5bb0acf
Felix Chen <felixchen3674@gmail.com>
2024-05-15 20:35:26 -0400

    homework solution example

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Day11Play from "./day11_describing_the_ui/notes";
 import Day12Play from "./day12_interactivity/notes";
+import Day13Play from "./day13_lifecycles/notes";
 
 function App() {
   return (
     <>
       {/* <Day11Play /> */}
-      <Day12Play />
+      {/* <Day12Play /> */}
+      <Day13Play />
     </>
   );
 }
