1266a4f0fdb56a7b9ab4882b73d81caf9b06c754
Lin He <xlhe@cs.washington.edu>
2024-05-16 16:27:53 -0700

    completed day14

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -4,25 +4,20 @@ import Day11Play from "./day11_describing_the_ui/notes";
 import Checkbox from "./day12_interactivity/ControlledComponents/Checkbox";
 import RadioGroup from "./day12_interactivity/ControlledComponents/RadioGroup";
 import Day12Play from "./day12_interactivity/notes";
-import Day13Play from "./day13_lifecycles/notes";
-import Day14Play from "./day14_practice";
-import Day16Play from "./day16_custom_hooks/notes";
-import Day17Play from "./day17_performance/notes";
-import Day21Play from "./day21_routing/notes";
-import Day22Play from "./day22_testing";
-
+import TodoListApp from "./day12_interactivity/TodoListApp";
+import Select from "./day12_interactivity/ControlledComponents/Select";
+import FetchPost from "./day13_lifecycles/FetchPost";
 function App() {
   return (
     <>
       {/* <Day11Play /> */}
       {/* <Day12Play /> */}
-      {/* <Day13Play /> */}
-      {/* <Day14Play /> */}
-      {/* <Day16Play /> */}
-      {/* <Day17Play /> */}
-      {/* <ReduxTodoSolution /> */}
-      {/* <Day21Play /> */}
-      <Day22Play />
+      {/* <TodoListApp /> */}
+      {/* <Checkbox /> */}
+      {/* <RadioGroup /> */}
+      {/* <Select /> */}
+      <FetchPost />
+
     </>
   );
 }
