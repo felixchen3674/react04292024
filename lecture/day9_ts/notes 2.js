b4c5261c50d9d7bb5eac0ccbc6bac76ccf89cff6
Lin He <xlhe@cs.washington.edu>
2024-05-16 13:01:51 -0700

    merge conflict

---
+++ lecture/day9_ts/notes 2.js
@@ -0,0 +1,9 @@
+//typescript is like a superset of js
+    //ts has everything js has, and more
+
+
+// interfaces vs types
+// most of the time, they are same except for syntax
+
+// my personal preference is: 
+// always use interfaces, until i have a need for type 
