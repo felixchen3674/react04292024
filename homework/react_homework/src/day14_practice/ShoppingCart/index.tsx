0699025bf9c3a6237aeb082d07c1599d2ecd7756
Yongjian Xuan <xcircle08@gmail.com>
2024-05-16 20:17:14 -0700

    update

--- homework/react_homework/src/day14_practice/ShoppingCart/index.tsx
+++ homework/react_homework/src/day14_practice/ShoppingCart/index.tsx
@@ -1,134 +1,62 @@
-// import { useState } from "react";
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
-
-// export default function ShoppingCart() {
-//   const [appleCount, setAppleCount] = useState(0);
-//   const [bananaCount, setBananaCount] = useState(0);
-//   const [mangoCount, setMangoCount] = useState(0);
-//   const [strawbeeryCount, setStrawberryCount] = useState(0);
-//   const [sum, setSum] = useState(0);
-
-//   const handleItemDecrement = (e) => {
-//     if (e.target.name === "banana") {
-//       if (bananaCount > 0) {
-//         setBananaCount((pre) => pre - 1);
-//       }
-//     } else if (e.target.name === "apple") {
-//       if (appleCount > 0) {
-//         setAppleCount((pre) => pre - 1);
-//       }
-//     } else if (e.target.name === "mango") {
-//       if (mangoCount > 0) {
-//         setMangoCount((pre) => pre - 1);
-//       }
-//     } else if (e.target.name === "strawberry") {
-//       if (strawbeeryCount > 0) {
-//         setStrawberryCount((pre) => pre - 1);
-//       }
-//     }
-//   };
-//   const handleItemIncrement = (e) => {
-//     if (e.target.name === "banana") {
-//       setBananaCount((pre) => pre + 1);
-//     } else if (e.target.name === "apple") {
-//       setAppleCount((pre) => pre + 1);
-//     } else if (e.target.name === "mango") {
-//       setMangoCount((pre) => pre + 1);
-//     } else if (e.target.name === "strawberry") {
-//       setStrawberryCount((pre) => pre + 1);
-//     }
-//   };
-//   return (
-//     <div>
-//       <h1>ShoppingCart</h1>
-//       <div>
-//         <table id="shoppingTable">
-//           <thead>
-//             <tr>
-//               <th>Name</th>
-//               <th>Price</th>
-//               <th>Action</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             <tr>
-//               <td>Apple</td>
-//               <td>$0.99</td>
-//               <td>
-//                 <button name="apple" onClick={handleItemDecrement}>
-//                   -
-//                 </button>
-//                 {appleCount}
-//                 <button name="apple" onClick={handleItemIncrement}>
-//                   +
-//                 </button>
-//               </td>
-//             </tr>
-//             <tr>
-//               <td>Banana</td>
-//               <td>$0.49</td>
-//               <td>
-//                 <button name="banana" onClick={handleItemDecrement}>
-//                   -
-//                 </button>
-//                 {bananaCount}
-//                 <button name="banana" onClick={handleItemIncrement}>
-//                   +
-//                 </button>
-//               </td>
-//             </tr>
-//             <tr>
-//               <td>Mango</td>
-//               <td>$1.99</td>
-//               <td>
-//                 <button name="mango" onClick={handleItemDecrement}>
-//                   -
-//                 </button>
-//                 {mangoCount}
-//                 <button name="mango" onClick={handleItemIncrement}>
-//                   +
-//                 </button>
-//               </td>
-//             </tr>
-//             <tr>
-//               <td>Strawberry</td>
-//               <td>$2.49</td>
-//               <td>
-//                 <button name="strawberry" onClick={handleItemDecrement}>
-//                   -
-//                 </button>
-//                 {strawbeeryCount}
-//                 <button name="strawberry" onClick={handleItemIncrement}>
-//                   +
-//                 </button>
-//               </td>
-//             </tr>
-//           </tbody>
-//         </table>
-//       </div>
-//       <div>
-//         <h1>Total Price:{sum}</h1>
-//       </div>
-//       <div>
-//         <button>checkout</button> <button>Empty Cart</button>
-//       </div>
-//     </div>
-//   );
-// }
-
-const items = [
-  { name: "Apple", price: 0.99 },
-  { name: "Banana", price: 0.49 },
-  { name: "Mango", price: 1.99 },
-  { name: "Strawberry", price: 2.49 },
+interface DataType {
+  id: number;
+  name: string;
+  price: number;
+  count: number;
+}
+const data: DataType[] = [
+  { id: 1, name: "Apple", price: 0.99, count: 0 },
+  { id: 2, name: "Banana", price: 0.49, count: 0 },
+  { id: 3, name: "Mango", price: 1.99, count: 0 },
+  { id: 4, name: "Strawberry", price: 2.49, count: 0 },
 ];
 
 export default function ShoppingCart() {
-  const [itemsCount, setItemsCount] = useState(items.map(() => 0));
+  const [items, setItems] = useState<DataType[]>([]);
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setItems(data);
+        } else {
+          throw new Error("something wrong");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  });
 
-  const handleItemDecrement = () => {};
-  const handleItemIncrement = () => {};
+  const handleItemDecrement = (id: number) => {
+    const newitems = items.map((item) => {
+      const newitem = { ...item };
+      if (newitem.id === id) {
+        if (newitem.count <= 0) {
+          return newitem;
+        }
+        newitem.count--;
+      }
+      return newitem;
+    });
+    setItems(newitems);
+  };
+  const handleItemIncrement = (id: number) => {
+    const newitems = items.map((item) => {
+      const newitem = { ...item };
+      if (newitem.id === id) {
+        newitem.count++;
+      }
+      return newitem;
+    });
+    setItems(newitems);
+  };
   return (
     <div>
       <h1>Shopping Cart</h1>
@@ -143,15 +71,19 @@ export default function ShoppingCart() {
           </thead>
           <tbody>
             {items &&
-              items.map((item, index) => {
+              items.map((item) => {
                 return (
-                  <tr key={item.name}>
+                  <tr key={item.id}>
                     <td>{item.name}</td>
                     <td>${item.price}</td>
                     <td>
-                      <button onClick={handleItemDecrement(index)}>-</button>
-                      {itemsCount[index]}
-                      <button onClick={handleItemIncrement(index)}>+</button>
+                      <button onClick={() => handleItemDecrement(item.id)}>
+                        -
+                      </button>
+                      {item.count}
+                      <button onClick={() => handleItemIncrement(item.id)}>
+                        +
+                      </button>
                     </td>
                   </tr>
                 );
