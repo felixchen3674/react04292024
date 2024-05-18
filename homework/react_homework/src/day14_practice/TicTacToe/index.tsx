76bf1a4c8cee281182de3a9b273b4c6440733464
Felix Chen <felixchen3674@gmail.com>
2024-05-17 23:17:21 -0400

    tic tac toe

--- homework/react_homework/src/day14_practice/TicTacToe/index.tsx
+++ homework/react_homework/src/day14_practice/TicTacToe/index.tsx
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 import React, {useState} from 'react'
 
 type Player = 'X' | 'O';
 type SquareValue = Player | null;
+=======
+import React from "react";
+import Solution from "./Solution";
+>>>>>>> ca451d1 (tic tac toe)
 
 export default function TicTacToe() {
   const [board, setBoard] = useState(initBoard);
