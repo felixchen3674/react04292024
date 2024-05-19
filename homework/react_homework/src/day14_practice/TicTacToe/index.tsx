8aabe5c65970bc50df7f2ad184463284989f515f
Lin He <xlhe@cs.washington.edu>
2024-05-19 10:31:47 -0700

    progress on ttt

--- homework/react_homework/src/day14_practice/TicTacToe/index.tsx
+++ homework/react_homework/src/day14_practice/TicTacToe/index.tsx
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React, {useState} from 'react'
 
 type Player = 'X' | 'O';
@@ -7,6 +8,16 @@ type SquareValue = Player | null;
 import React from "react";
 import Solution from "./Solution";
 >>>>>>> ca451d1 (tic tac toe)
+=======
+import React from "react";
+import Solution from "./Solution";
+=======
+import React, {useState} from 'react'
+
+type Player = 'X' | 'O';
+type SquareValue = Player | null;
+>>>>>>> c912a75 (progress on ttt)
+>>>>>>> a72aefb (progress on ttt)
 
 export default function TicTacToe() {
   return (
