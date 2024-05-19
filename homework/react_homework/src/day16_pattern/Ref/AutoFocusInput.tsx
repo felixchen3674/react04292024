0c8f884c98892a53f38578af23ef0178a4a7b5bd
Felix Chen <felixchen3674@gmail.com>
2024-05-19 12:06:10 -0400

    day 16 homework

---
+++ homework/react_homework/src/day16_pattern/Ref/AutoFocusInput.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function AutoFocusInput() {
+  return (
+    <div>
+      <div>This input element should be auto focused when page loads</div>
+      <input />
+    </div>
+  );
+}
