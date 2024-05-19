4d83bf2194899938134d51c7110f94ba9a994d92
Felix Chen <felixchen3674@gmail.com>
2024-05-19 12:06:10 -0400

    day 16 homework

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -3,6 +3,7 @@ import Day11Play from "./day11_describing_the_ui/notes";
 import Day12Play from "./day12_interactivity/notes";
 import Day13Play from "./day13_lifecycles/notes";
 import Day14Play from "./day14_practice";
+import Day16Play from "./day16_pattern/notes";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <Day11Play /> */}
       {/* <Day12Play /> */}
       {/* <Day13Play /> */}
-      <Day14Play />
+      {/* <Day14Play /> */}
+      <Day16Play />
     </>
   );
 }
---
+++ homework/react_homework/src/day16_pattern/CustomHooks/Fetch/Todos.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// use useFetch to implement the Component
+// fetches from jsonplaceholder, implement loading, and error handling
+
+export default function Todos() {
+  return (
+    <div>Todos</div>
+  )
+}
