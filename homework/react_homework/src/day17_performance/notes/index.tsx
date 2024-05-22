66e8b9600158d14f3a682869b342944aa4b3f29f
ziyang-666 <song.ziya@northeastern.edu>
2024-05-22 00:21:44 -0700

    update 17

--- homework/react_homework/src/day17_performance/notes/index.tsx
+++ homework/react_homework/src/day17_performance/notes/index.tsx
@@ -5,8 +5,8 @@ export default function Day17Play() {
   return (
     <div>
       <h1>Day 17 Play</h1>
-      {/* <BirthdayRecord /> */}
-      <DebouncedCallback />
+      <BirthdayRecord />
+      {/* <DebouncedCallback /> */}
     </div>
   );
 }
