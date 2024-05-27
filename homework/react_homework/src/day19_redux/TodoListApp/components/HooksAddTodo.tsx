367bc493ae2f0dc8295abd53de7364bf33f477a0
LiZhe <1756368629@qq.com>
2024-05-27 12:08:02 -0400

    day20

--- homework/react_homework/src/day19_redux/TodoListApp/components/HooksAddTodo.tsx
+++ homework/react_homework/src/day19_redux/TodoListApp/components/HooksAddTodo.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { addTodo } from '../redux/todoitems/todoActions';
 import HooksTodoList from './HooksTodoList';
