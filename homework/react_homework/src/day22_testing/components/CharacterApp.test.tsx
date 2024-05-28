67983b7f4761621e80d1b4e78c1ab33c03afd2c8
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-05-28 15:35:07 -0700

    2nd

--- homework/react_homework/src/day22_testing/components/CharacterApp.test.tsx
+++ homework/react_homework/src/day22_testing/components/CharacterApp.test.tsx
@@ -1,16 +1,81 @@
-import { render, fireEvent, screen } from "@testing-library/react";
-import CharactersApp from "./CharactersApp";
+import {render, fireEvent, screen, waitFor} from "@testing-library/react"
+import CharactersApp from "./CharactersApp"
 
-import { describe, expect, test } from "vitest";
+import {describe, expect, test, vi, beforeEach} from "vitest"
+import {getCharacters} from "../api/get-characters"
+
+vi.mock("../api/get-characters")
 
 describe("<CharactersApp />", () => {
+  beforeEach(() => {
+    ;(getCharacters as jest.Mock).mockImplementation(
+      async (pageNumber: number) => {
+        return Array.from({length: 10}, (_, i) => ({
+          url: `https://www.anapioficeandfire.com/api/characters/${
+            i + 1 + (pageNumber - 1) * 10
+          }`,
+          name: `Character ${i + 1 + (pageNumber - 1) * 10}`,
+          gender: "Male",
+          culture: "Culture",
+          born: "In 283 AC",
+          died: "",
+          titles: ["Title"],
+          aliases: ["Alias"],
+          father: "",
+          mother: "",
+          spouse: "",
+          allegiances: [],
+          books: [],
+          povBooks: [],
+          tvSeries: [],
+          playedBy: [],
+        }))
+      }
+    )
+  })
+
   describe("loading character", () => {
-    test('renders the title "Characters"', async () => {});
-    test("renders a list of 10 characters", async () => {});
-  });
+    test('renders the title "Characters"', async () => {
+      render(<CharactersApp />)
+      expect(
+        screen.getByRole("heading", {name: /Characters/i})
+      ).toBeInTheDocument()
+    })
+    test("renders a list of 10 characters", async () => {
+      render(<CharactersApp />)
+
+      await waitFor(() => {
+        const characterElements = screen.getAllByText(/Character \d+/)
+        expect(characterElements.length).toBe(10)
+      })
+    })
+  })
+
   describe("loading more characters", () => {
-    test('has a "Load More Characters" button', async () => {});
-    test("clicking load more gets 10 more characters", async () => {});
-    test("clicking load more increases the page number", async () => {});
-  });
-});
+    test('has a "Load More Characters" button', async () => {
+      render(<CharactersApp />)
+      expect(screen.getByText(/Load More Characters/)).toBeInTheDocument()
+    })
+
+    test("clicking load more gets 10 more characters", async () => {
+      render(<CharactersApp />)
+
+      fireEvent.click(screen.getByText(/Load More Characters/))
+
+      await waitFor(() => {
+        const characterElements = screen.getAllByText(/Character \d+/)
+        expect(characterElements.length).toBe(20)
+      })
+    })
+
+    test("clicking load more increases the page number", async () => {
+      render(<CharactersApp />)
+
+      fireEvent.click(screen.getByText(/Load More Characters/))
+
+      await waitFor(() => {
+        expect(screen.getByText(/Next Page: 2/)).toBeInTheDocument()
+      })
+    })
+  })
+})
