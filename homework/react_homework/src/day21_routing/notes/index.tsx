751d22c836aaa36e66647f2f8d8bbbed31073bc5
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-05-28 00:26:17 -0700

    push before heading to bed

--- homework/react_homework/src/day21_routing/notes/index.tsx
+++ homework/react_homework/src/day21_routing/notes/index.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Homepage from "./Homepage";
+import React from "react"
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom"
+import Homepage from "./Homepage"
 
 // set up your routes here
 export default function Day21Play() {
-  return <Router>
-    <Switch>
-        <Route path="/">
-            <Homepage />
-        </Route>
-    </Switch>
-  </Router>;
+  return <Homepage />
+  // return (
+  //   <Router>
+  //     <Switch>
+  //       <Route path="/">
+  //         <Homepage />
+  //       </Route>
+  //     </Switch>
+  //   </Router>
+  // )
 }
