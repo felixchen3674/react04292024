d86a891a2f9384a83500850dd60b2a3d0069559e
tchen285 <91436200+tchen285@users.noreply.github.com>
2024-05-28 17:28:47 -0700

    update

--- homework/react_homework/src/day22_testing/components/Character.test.tsx
+++ homework/react_homework/src/day22_testing/components/Character.test.tsx
@@ -2,73 +2,30 @@ import {describe, expect, test} from "vitest"
 import {CharacterType} from "../types/characterType"
 import Character from "./Character"
 import {render, screen} from "@testing-library/react"
-import React from "react"
+import characters from "../../testing/mock/day22Mock/characters"
 
-const mockCharacter: CharacterType = {
-  url: "https://www.anapioficeandfire.com/api/characters/583",
-  name: "Jon Snow",
-  gender: "Male",
-  culture: "Northmen",
-  born: "In 283 AC",
-  died: "",
-  titles: ["Lord Commander of the Night's Watch"],
-  aliases: [
-    "Lord Snow",
-    "Ned Stark's Bastard",
-    "The Snow of Winterfell",
-    "The Crow-Come-Over",
-    "The 998th Lord Commander of the Night's Watch",
-    "The Bastard of Winterfell",
-    "The Black Bastard of the Wall",
-    "Lord Crow",
-  ],
-  father: "",
-  mother: "",
-  spouse: "",
-  allegiances: ["https://www.anapioficeandfire.com/api/houses/362"],
-  books: ["https://www.anapioficeandfire.com/api/books/5"],
-  povBooks: [
-    "https://www.anapioficeandfire.com/api/books/1",
-    "https://www.anapioficeandfire.com/api/books/2",
-    "https://www.anapioficeandfire.com/api/books/3",
-    "https://www.anapioficeandfire.com/api/books/8",
-  ],
-  tvSeries: [
-    "Season 1",
-    "Season 2",
-    "Season 3",
-    "Season 4",
-    "Season 5",
-    "Season 6",
-  ],
-  playedBy: ["Kit Harington"],
-}
-
-const mockCharacterWithoutName: CharacterType = {
-  ...mockCharacter,
-  name: "",
-}
+const mockCharacter: CharacterType = characters[1]
+const mockCharacterWithoutName: CharacterType = characters[0]
 
 describe("<Character />", () => {
   test('shows fields for "name" and "culture"', async () => {
     render(<Character character={mockCharacter} />)
-    expect(screen.getByText(/Jon Snow/)).toBeInTheDocument()
-    expect(screen.getByText(/Northmen/)).toBeInTheDocument()
+    expect(screen.getByText(/Walder/)).toBeInTheDocument()
   })
 
   test("shows culture if it is present", async () => {
-    render(<Character character={mockCharacter} />)
-    expect(screen.getByText(/Northmen/)).toBeInTheDocument()
+    render(<Character character={mockCharacterWithoutName} />)
+    expect(screen.getByText(/Braavosi/)).toBeInTheDocument()
   })
 
   test("shows alias if no name is present", async () => {
     render(<Character character={mockCharacterWithoutName} />)
-    expect(screen.getByText(/Lord Snow/)).toBeInTheDocument()
+    expect(screen.getByText(/The Daughter of the Dusk/)).toBeInTheDocument()
   })
 
   test("shows how many books this characters made an appearance in", async () => {
     render(<Character character={mockCharacter} />)
     expect(screen.getByText(/Number of Books:/)).toBeInTheDocument()
-    expect(screen.getByText("1")).toBeInTheDocument()
+    expect(screen.getByText("5")).toBeInTheDocument()
   })
 })
