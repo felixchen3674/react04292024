914734ab0b4602cea0b8e83033b9b5ee967572cd
Lin He <xlhe@cs.washington.edu>
2024-05-28 17:56:40 -0700

    completed hw with 3 failing test

--- homework/react_homework/src/day22_testing/components/Character.test.tsx
+++ homework/react_homework/src/day22_testing/components/Character.test.tsx
@@ -1,8 +1,38 @@
 import { describe, expect, test } from "vitest";
+import { CharacterType } from "../types/characterType";
+import { getCharacters } from "../api/get-characters";
 
 describe("<Character />", () => {
-  test('shows fields for "name" and "culture"', async () => {});
-  test("shows culture if it is present", async () => {});
-  test("shows alias if no name is present", async () => {});
-  test("shows how many books this characters made an appearance in", async () => {});
+  test('shows fields for "name" and "culture"', async () => {
+    const characters = await getCharacters();
+    const character = characters[0];
+    expect(character).toHaveProperty("name");
+    expect(character).toHaveProperty("culture");
+  });
+
+  test("shows culture if it is present", async () => {
+    const characters = await getCharacters();
+    const character = characters[0];
+    if (character.culture) {
+      expect(character).not.toBe('');
+    }
+  });
+
+  test("shows alias if no name is present", async () => {
+    const characters = await getCharacters();
+    // create a new object with name entities altered to empty to simulate the 
+    // situation that no name exists
+    const character = {
+      ...characters[0],
+      name : "",
+    }
+    if (!character.name) {
+      expect(character.aliases.length).greaterThan(0);
+    }
+  });
+  test("shows how many books this characters made an appearance in", async () => {
+    const characters = await getCharacters();
+    const character = characters[0];
+    expect(character.books.length).not.toBe(0);
+  });
 });
--- homework/react_homework/src/day22_testing/components/CharacterApp.test.tsx
+++ homework/react_homework/src/day22_testing/components/CharacterApp.test.tsx
@@ -1,16 +1,49 @@
-import { render, fireEvent, screen } from "@testing-library/react";
+import { render, fireEvent, screen, waitFor } from "@testing-library/react";
 import CharactersApp from "./CharactersApp";
 
 import { describe, expect, test } from "vitest";
 
 describe("<CharactersApp />", () => {
   describe("loading character", () => {
-    test('renders the title "Characters"', async () => {});
-    test("renders a list of 10 characters", async () => {});
+    test('renders the title "Characters"', async () => {
+      render(<CharactersApp />);
+      expect(screen.getByText("Characters")).toBeInTheDocument();
+
+    });
+    test("renders a list of 10 characters", async () => {
+      render(<CharactersApp />);
+      
+      await waitFor(() => {
+        const characters = screen.getAllByText('Character \d+');
+        expect(characters.length).toBe(10);
+      })
+      
+    });
   });
   describe("loading more characters", () => {
-    test('has a "Load More Characters" button', async () => {});
-    test("clicking load more gets 10 more characters", async () => {});
-    test("clicking load more increases the page number", async () => {});
+    test('has a "Load More Characters" button', async () => {
+      render(<CharactersApp />);
+      expect(screen.getByText("Load More Characters")).toBeInTheDocument();
+    });
+    test("clicking load more gets 10 more characters", async () => {
+      render(<CharactersApp />);
+      const button = screen.getByText("Load More Characters");
+      fireEvent.click(button);
+
+      await waitFor (() => {
+        const characters = screen.getAllByText("Character \d+");
+        expect(characters.length).toBe(10);
+      })
+    });
+    test("clicking load more increases the page number", async () => {
+      render(<CharactersApp />);
+      const button = screen.getByText("Load More Characters");
+      fireEvent.click(button);
+
+      await waitFor(() => {
+        const nextPage = screen.getByText("Next Page: 2");
+        expect(nextPage).toBeInTheDocument();
+      })
+    });
   });
 });
