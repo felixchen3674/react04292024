cbf26a6232902fc1c5fbe2654869df59183823a1
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-03 11:38:29 -0700

    Update App.tsx

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -1,25 +1,32 @@
-import "./App.css"
-import Day11Play from "./day11_describing_the_ui/notes"
-import Day12Play from "./day12_interactivity/notes"
-import Day13Play from "./day13_lifecycles/notes"
-import Day14Play from "./day14_practice"
-import Day16Play from "./day16_custom_hooks/notes"
-import Day17Play from "./day17_performance/notes"
-import Day19Play from "./day19_redux1/note"
-import Day21Play from "./day21_routing/notes"
+import './App.css'
+
+import ReduxTodoSolution from './ReduxTodoSolution'
+
+import Day12Play from './day12_interactivity/notes'
+import Day13Play from './day13_lifecycles/notes'
+
+import Day19Play from './day19_redux1/note'
+
+import Day21Play from './day21_routing/notes'
 
 function App() {
   return (
     <>
       {/* <Day11Play /> */}
       {/* <Day12Play /> */}
-      {/* <Day13Play /> */}
+      <Day13Play />
       {/* <Day14Play /> */}
       {/* <Day16Play /> */}
+
       {/* <Day17Play /> */}
+      {/* <Day11Play />
+
+      <Day17Play /> */}
 
       {/* <Day19Play /> */}
-      <Day21Play />
+
+      {/* <Day19Play /> */}
+      {/* <Day21Play /> */}
     </>
   )
 }
