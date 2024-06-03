ae576c3b2adc077cead8a885c5d3ff037c16a9ab
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-02 23:38:05 -0700

    Update Checkbox.tsx

--- homework/react_homework/src/day12_interactivity/ControlledComponents/Checkbox.tsx
+++ homework/react_homework/src/day12_interactivity/ControlledComponents/Checkbox.tsx
@@ -1,3 +1,21 @@
-export default function Checkbox() {
-  return <div>Checkbox</div>;
+import {useState} from 'react'
+
+export default function CheckBox(): JSX.Element {
+  const [isChecked, setIsChecked] = useState(false)
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setIsChecked(e.target.checked)
+  }
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheckboxChange}
+      />
+      <label>Label</label>
+      <p>Label is checked: {isChecked.toString()}</p>
+    </div>
+  )
 }
