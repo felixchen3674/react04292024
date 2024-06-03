f568927d2e2123d06db97855ca24b5f1a18d28cb
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-02 23:39:48 -0700

    Update RadioGroup.tsx

--- homework/react_homework/src/day12_interactivity/ControlledComponents/RadioGroup.tsx
+++ homework/react_homework/src/day12_interactivity/ControlledComponents/RadioGroup.tsx
@@ -1,5 +1,48 @@
-export default function RadioGroup() {
+import {useState} from 'react'
+
+export default function RadioGroup(): JSX.Element {
+  const [grouped, setGrouped] = useState('Female')
+
+  const handleRadioGroupChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setGrouped(e.target.value)
+  }
+
   return (
-    <div>RadioGroup</div>
+    <div>
+      <p>Gender</p>
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="Female"
+          checked={grouped === 'Female'}
+          onChange={handleRadioGroupChange}
+        />
+        Female
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="Male"
+          checked={grouped === 'Male'}
+          onChange={handleRadioGroupChange}
+        />
+        Male
+      </label>
+      <br />
+      <label>
+        <input
+          type="radio"
+          name="gender"
+          value="Other"
+          checked={grouped === 'Other'}
+          onChange={handleRadioGroupChange}
+        />
+        Other
+      </label>
+      <p>Selected: {grouped}</p>
+    </div>
   )
 }
