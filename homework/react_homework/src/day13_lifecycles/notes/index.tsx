93c20546ddc561c3b5d70342821c1a3fbeeefa30
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-03 11:37:34 -0700

    Update index.tsx

--- homework/react_homework/src/day13_lifecycles/notes/index.tsx
+++ homework/react_homework/src/day13_lifecycles/notes/index.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import FetchPostByIdApp from "../solutions/FetchPostByIdApp";
+import React from 'react'
+import FetchPostByIdApp from '../solutions/FetchPostByIdApp'
+import FetchById from './FetchById'
 
 export default function Day13Play() {
   return (
     <div>
       <h1>Day 13 Play</h1>
       <div>
-        <FetchPostByIdApp />
+        <FetchById />
       </div>
     </div>
-  );
+  )
 }
