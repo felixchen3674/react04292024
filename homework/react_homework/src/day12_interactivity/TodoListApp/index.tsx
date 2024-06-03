caa256b7a3ea3c0e74d24225ffee9660d009027d
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-02 23:30:34 -0700

    Update index.tsx

--- homework/react_homework/src/day12_interactivity/TodoListApp/index.tsx
+++ homework/react_homework/src/day12_interactivity/TodoListApp/index.tsx
@@ -1,4 +1,54 @@
+import {useState} from 'react'
+
 export default function TodoListApp() {
   // write your todo list app here
-  return <div>TodoListApp</div>;
+  const [todos, setTodos] = useState([])
+  const [newTodo, setNewTodo] = useState('')
+
+  function addTodo() {
+    if (newTodo.trim() === '') {
+      return
+    }
+
+    const newTodoItem = {
+      id: todos.length + 1,
+      text: newTodo,
+      completed: false,
+    }
+
+    setTodos((todos) => [...todos, newTodoItem])
+    setNewTodo('')
+  }
+
+  function deleteTodo(id) {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id))
+  }
+
+  return (
+    <>
+      <h2>TodoListApp</h2>
+      <input
+        type="text"
+        placeholder="add new..."
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>ADD</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}{' '}
+            <button
+              onClick={(e) => {
+                e.stopPropagation()
+                deleteTodo(todo.id)
+              }}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
