fb3907ab26dec1015b83068666493e5fcd256fe7
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-02 23:29:56 -0700

    Update index.tsx

--- homework/react_homework/src/day12_interactivity/notes/index.tsx
+++ homework/react_homework/src/day12_interactivity/notes/index.tsx
@@ -1,9 +1,13 @@
+import TodoListApp from '../TodoListApp'
+
 export default function Day12Play() {
-    return (
-      <div>
-        <h1>Day 122 Notes</h1>
-        {/* This is your play around code */}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Day 12 Notes</h1>
+      {/* This is your play around code */}
+      <TodoListApp />
+    </div>
+  )
+}
+
   
