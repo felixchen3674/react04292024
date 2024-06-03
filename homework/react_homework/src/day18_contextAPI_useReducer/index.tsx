78bec04a889d605b45b22c713e13cd971844922c
Tao Chen <91436200+tchen285@users.noreply.github.com>
2024-06-03 13:02:55 -0700

    Update index.tsx

--- homework/react_homework/src/day18_contextAPI_useReducer/index.tsx
+++ homework/react_homework/src/day18_contextAPI_useReducer/index.tsx
@@ -1,1 +1,14 @@
-// felix: day 18 homework?
+import React from 'react'
+import TodoList from './todos'
+import {TodosProvider} from './TodosContext'
+
+export default function Day18Play() {
+  return (
+    <div>
+      <h1>Day 18 Play</h1>
+      <TodosProvider>
+        <TodoList />
+      </TodosProvider>
+    </div>
+  )
+}
