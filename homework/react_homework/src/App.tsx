b6fc6dd0ed24b56ada15e850574553f21b1272cc
Lin He <xlhe@cs.washington.edu>
2024-06-05 11:51:25 -0700

    menu list

--- homework/react_homework/src/App.tsx
+++ homework/react_homework/src/App.tsx
@@ -1,13 +1,6 @@
 import "./App.css";
 import ReduxTodoSolution from "./ReduxTodoSolution";
 import Day11Play from "./day11_describing_the_ui/notes";
-import Checkbox from "./day12_interactivity/ControlledComponents/Checkbox";
-import RadioGroup from "./day12_interactivity/ControlledComponents/RadioGroup";
-import TodoListApp from "./day12_interactivity/TodoListApp";
-import Select from "./day12_interactivity/ControlledComponents/Select";
-import FetchPost from "./day13_lifecycles/FetchPost";
-import SelectAllForm from "./day14_practice/SelectAllForm";
-import ShoppingCart from "./day14_practice/ShoppingCart";
 import Day12Play from "./day12_interactivity/notes";
 import Day13Play from "./day13_lifecycles/notes";
 import Day14Play from "./day14_practice";
@@ -15,14 +8,36 @@ import Day16Play from "./day16_custom_hooks/notes";
 import Day17Play from "./day17_performance/notes";
 import Day21Play from "./day21_routing/notes";
 import Day22Play from "./day22_testing";
-import ColorPicker from "./many_practices/ColorPicker";
+import MenuList from "./many_practices/MenuList";
 
 function App() {
-  const colorOptions : string[] = ['red', 'green', 'blue', 'orange', 'purple'];
+  const menuConfig = [
+    {
+      title: 'Home',
+    },
+    {
+      title: 'Services',
+      subItems: ['Cooking', 'Cleaning'],
+    },
+    {
+      title: 'Contact',
+      subItems: ['Phone', 'Mail'],
+    },
+  ];
+  
   return (
     <>
-
-      <ColorPicker initialSelectedColor="red" colorPickerOptions={colorOptions} />
+      {/* <Day11Play /> */}
+      {/* <Day12Play /> */}
+      {/* <Day13Play /> */}
+      {/* <Day14Play /> */}
+      {/* <Day16Play /> */}
+      {/* <Day17Play /> */}
+      {/* <ReduxTodoSolution /> */}
+      {/* <Day21Play /> */}
+      {/* <Day22Play /> */}
+      <MenuList menuConfig={menuConfig} />
+      
     </>
   );
 }
--- homework/react_homework/src/many_practices/MenuList/index.tsx
+++ homework/react_homework/src/many_practices/MenuList/index.tsx
@@ -1,5 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function MenuList() {
-  return <div>MenuList</div>;
+interface MenuItem {
+  title: string;
+  subItems?: string[];
 }
+
+interface SideMenuProps {
+  menuConfig: MenuItem[];
+}
+
+const MenuList: React.FC<SideMenuProps> = ({ menuConfig }) => {
+  const [expandedMenu, setExpandedMenu] = useState<string | null>(null);
+  const handleExpand = (title: string) => {
+    // check if the title clicked is the current title, if it is, collpase it, if not, expand
+    setExpandedMenu(expandedMenu === title ? null : title);
+  };
+
+  return (
+    <div className="menu-wrapper">
+      {menuConfig.map((item) => (
+        <div
+          key={item.title}
+          data-test-id={`first-level-${item.title.toLowerCase()}`}
+        >
+          <div>{item.title}</div>
+          {/* checks if the subitem exists, and if there's actual content in it */}
+          {item.subItems && item.subItems.length > 0 && (
+            <>
+              <button
+                data-test-id={`button-${item.title.toLowerCase()}`}
+                onClick={() => handleExpand(item.title)}
+              >
+                {expandedMenu === item.title ? "hide" : "expand"}
+              </button>
+              {/* render the menu */}
+              {expandedMenu === item.title && (
+                <ul data-test-id={`ul-${item.title.toLowerCase()}`}>
+                  {item.subItems.map((subItem) => (
+                    <li
+                      key={subItem}
+                      data-test-id={`li-${item.title.toLowerCase()}-${subItem.toLowerCase()}`}
+                    >
+                      {subItem}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default MenuList;
